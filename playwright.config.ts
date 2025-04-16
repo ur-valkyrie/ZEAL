73aa66be7afda35180b9c2777e1771b01a666160
ur-valkyrie <castiel.andreeva@gmail.com>
2025-04-16 13:34:50 +0200

    Added Playwright CI/CD pipeline;

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,11 +20,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    }
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // }
     // {
     //   name: "Mobile Chrome",
     //   use: { ...devices["Pixel 5"] },
